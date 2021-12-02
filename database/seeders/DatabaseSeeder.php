1549986dac56c20ff7092f94d7ddb082c71685d9
dmica <dmitrovic.maja@gmail.com>
2021-12-02 16:58:08 +0000

    adding factories for questions and users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Question;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->count(3)->create();
+        Question::factory()->count(10)->create();
     }
 }
