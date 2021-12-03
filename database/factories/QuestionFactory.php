9e7694eee5111b605123e78752d5b5826d5709b8
dmica <dmitrovic.maja@gmail.com>
2021-12-03 15:44:16 +0000

    fixed migration bug

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -17,12 +17,10 @@ class QuestionFactory extends Factory
         return [
             'title'     => $this->faker->sentences(1, true),
             'body'      => $this->faker->paragraphs(1, true),
-            'views'     => $this->faker->numberBetween(1,25),
-            'answers'   => $this->faker->sentences(3, true),
-            'votes'     => $this->faker->unique(true)->numberBetween(1, 50),
             'user_id'   => User::inRandomOrder()->first()->id,
-            // 'created_at' => now(),
-            // 'updated_at' => now(),
+            'views'     => $this->faker->numberBetween(1,25),
+            'answers'   => $this->faker->unique(true)->numberBetween(0, 10),
+            'votes'     => $this->faker->unique(true)->numberBetween(1, 50),
         ];
     }
 }
